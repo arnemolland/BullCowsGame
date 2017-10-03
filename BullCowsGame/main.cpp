e1496afb7b8d33ad87566c940479c684f619a406
Arne Olai Lyngsmo Molland <arneolaim@gmail.com>
2017-10-03 11:38:44 +0200

    Initial Commit

---
+++ BullCowsGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BullCowsGame
+//
+//  Created by Arne Molland on 03/10/2017.
+//  Copyright © 2017 Arne Molland. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
