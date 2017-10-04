5a9d3e6c2839ab09c919160e6fa323175a20911c
Arne Olai Lyngsmo Molland <arneolaim@gmail.com>
2017-10-04 13:50:36 +0200

    Input

--- BullCowsGame/main.cpp
+++ BullCowsGame/main.cpp
@@ -6,14 +6,28 @@
 //  Copyright © 2017 Arne Molland. All rights reserved.
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
+    // Initialize a constexpr value
     constexpr int WORD_LENGTH = 55;
+    
+    //Introduce the game
     cout << "Welcome to Bulls and Cows\n";
     cout << "Can you guess the " << WORD_LENGTH;
     cout << " letter isogram?\n";
+    
+    // Get input from user
+    string Guess = "";
+    cout << "Enter your guess: ";
+    cin >> Guess;
+    
+
+    
+    
+    // Repeat inpot back to user
     return 0;
 }
 
